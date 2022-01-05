d3c42925488417fd92abc8e9bacd5ff87baf1cf8
FABIO SANTOS NOVAIS <44532656+FsnRorschach@users.noreply.github.com>
2022-01-04 21:21:19 -0300

    Projeto 7 - Desafio 7 - Exercicios 8 - Projeto 8

--- level 7/Projeto 1 Level 7/script.js
+++ level 7/Projeto 1 Level 7/script.js
@@ -1,6 +1,8 @@
 let arrayimg = [];
 let conto = '';
 
+let textconto = document.getElementById('ptextConto');
+
 document.querySelector('button').addEventListener('click', evt => {
     evt.preventDefault();
     const nome = document.getElementById('nick').value;
@@ -12,7 +14,11 @@ document.querySelector('button').addEventListener('click', evt => {
     const objHist = new historiaObj(nome, personagem, monstros, armas, mapa, tragedia);
     montaMapa(objHist);
     montaPersonagem(objHist);
-    console.log(conto);
+    montaTragedia(objHist);
+    montarArmas(objHist);
+    mostraModal();
+    montaBackground();
+    maquinaEscrever();
 });
 
 class historiaObj {
@@ -29,22 +35,18 @@ class historiaObj {
 }
 
 function montaMapa(obj) {
-    console.log(obj);
-
-    // "Era uma vez em lugar chamado Disney World havia lá um habitante que se chamava Otávio, ele era um golfinho muito alegre, até que foi chegada a era dos Zumbis, que foi causada em 2021 (iremos utilizar aqui date.getFullYear() para sempre retornar o ano que o usuário se encontra) por uma fissura nuclear que fazia com que os humanos pertos se tornassem Zumbis, criaturas sem qualquer tipo de consciência, suas únicas motivações eram comer golfinhos e dançar break. O golfinho se viu obrigado a aprender técnicas de combate aprimoradas, até que encontrou no Submundo de Disney World uma varinha mágica, ferramenta que ele utilizou por décadas no combate destes Zumbis, até que um dia aprendeu uma magia capaz de trazer os Zumbis de volta a sanidade mental, esses por sua vez não esqueceram como dançar break... Fim".
-
     switch (obj.mapa) {
         case "gelo":
             conto += `Era uma vez em uma terra distante de clima tropical, morava ${obj.nome} com sua familia feliz, `;
             break;
         case "refugio":
-            conto += `Era uma vez em uma ilha distante da civilização, onde existia uma enorme montanha, morava ${obj.nome} , só em sua jornada espiritual, `;
+            conto += `Era uma vez em uma ilha distante da civilização, onde existia uma enorme montanha, morava ${obj.nome}, só, em sua jornada espiritual, `;
             break;
         case "fogo":
-            conto += `Era uma vez em no Deserto da Saara, junto ao seu povo viajava ${obj.nome} a procura de um local para descansar, `;
+            conto += `Era uma vez no Deserto da Saara, junto ao seu povo viajava ${obj.nome} a procura de um local para descansar, `;
             break;
         case "cidade":
-            conto += `Era uma vez em no cidade mais populasa do Brasil São Paulo, vivia ${obj.nome} com sua vida pacata e rotineira, `;
+            conto += `Era uma vez na cidade mais populasa do Brasil, São Paulo, vivia ${obj.nome} com sua vida pacata e rotineira, `;
             break;
         default:
             break;
@@ -52,7 +54,6 @@ function montaMapa(obj) {
 }
 
 function montaPersonagem(obj) {
-    console.log(obj);
     let date = new Date();
 
     switch (obj.personagem) {
@@ -60,13 +61,13 @@ function montaPersonagem(obj) {
             conto += `ele era um aprendiz de ${obj.personagem}, que fazia truques para sobreviver, quando no ano de ${date.getFullYear()} aconteceu  `;
             break;
         case "tanker":
-            conto += `ele era um aspirante a lutador de Box, treinava enquanto exercia a função de ${obj.personagem} no exército, quando no ano de ${date.getFullYear()} veio o chamado `;
+            conto += `ele era um aspirante a lutador de Box, treinava enquanto exercia a função de ${obj.personagem} no exército, quando no ano de ${date.getFullYear()} veio o chamado, `;
             break;
         case "princesa":
-            conto += `a ${obj.personagem} vivia em seu mundo de conto de fadas, sempre alegre e disposta ajudar, quando no ano de  ${obj.personagem} seu mundo desabou, `;
+            conto += `a ${obj.personagem} vivia em seu mundo de conto de fadas, sempre alegre e disposta a ajudar, quando no ano de  ${obj.personagem} seu mundo desabou, `;
             break;
         case "guerreiro":
-            conto += `o ávido ${obj.personagem} parecia apenas se importar com sua força e destreza, quando em meados do ano ${date.getFullYear()} aconteceu`;
+            conto += `o ávido ${obj.personagem} parecia apenas se importar com sua força e destreza, quando em meados do ano ${date.getFullYear()} aconteceu, `;
             break;
         default:
             break;
@@ -74,22 +75,91 @@ function montaPersonagem(obj) {
 }
 
 function montaTragedia(obj) {
-    console.log(obj);
-
     switch (obj.tragedia) {
         case "temporal":
-            conto += `uma ${obj.tragedia} rompeu a fina camada de frequência que mantinha o universo separado por camadas, dessa forma portais temporais começaram a abrir e se espalhar pela Terra, engolindo e trazendo humanos e outros seres de outras épocas e lugares, o pânico se instalou.`;
+            conto += `uma Distorção ${obj.tragedia} rompeu a fina camada de frequência que mantinha os universos separados, dessa forma portais temporais começaram a abrir e se espalhar pela Terra e Universo, engolindo e trazendo humanos e outros seres de outras épocas e lugares, o pânico se instalou. Mas uma ponta de esperança surgiu de onde menos se esperava.`;
+            arrayimg.push(`img/rupturaTemporal1.jpg`);
+            arrayimg.push(`img/rupturaTemporal2.jpg`);
+            arrayimg.push(`img/rupturaTemporal3.jpg`);
+            arrayimg.push(`img/rupturaTemporal4.jpg`);
+            arrayimg.push(`img/rupturaTemporal5.jpg`);
             break;
         case "desmagnetizacao":
-            conto += `${obj.tragedia} devido ao começo de expedições lunares a lua não aguentou a intervenção terraquea na exploração de minerais e partiu e 3 pedaçõs, a terra começou a gradualmente perder o eixo de rotação, devido a isso saiu 3 graus da rotação solar, causando um descontrole de perca atmosferica, reduzindo a população de seres vivos para 15%.`;
+            conto += `então aconteceu a ${obj.tragedia} polar devido ao começo de expedições lunares a lua não aguentou a intervenção terráquea na exploração de minerais e partiu e 3 pedaçõs, a terra começou a gradualmente perder o eixo de rotação, devido a isso saiu 3 graus da rotação solar, causando um descontrole de perca atmosferica, reduzindo a população de seres vivos para 15%. mas nem tudo estava perdido, os poucos sobreviventes encontraram uma forma de sobreviver com esperança de um futuro melhor.`;            
+            arrayimg.push(`img/campo1.jpg`);
+            arrayimg.push(`img/campo2.jpg`);
+            arrayimg.push(`img/campo3.jpg`);
+            arrayimg.push(`img/campo4.jpg`);
+            arrayimg.push(`img/campo5.jpg`);
+            arrayimg.push(`img/campo6.jpeg`);
             break;
         case "erupcaoSolar":
-            conto += `o Planeta Mercúrio foi puxado pela gravidade do Sol, o impacto gerou força o suficiente para causar uma variação de matéria escura, os danos foram uma esplosão tão forte que causou uma ${obj.tragedia}, o dia ficou escaldante, não existe mais vegetação, o que restou de seres vivos, vivem no subterrâneo.`;
+            conto += `o Planeta Mercúrio foi puxado pela gravidade do Sol, o impacto gerou força o suficiente para causar uma variação de matéria escura, os danos foram uma esplosão tão forte que causou uma ${obj.tragedia}, o dia ficou escaldante, não existe mais vegetação, o que restou de seres vivos, vivem no subterrâneo. Mas a humanidade havia se preparado para esse momento, nem tudo estava perdido.`;
+            arrayimg.push(`img/terraDevastada1.jpg`);
+            arrayimg.push(`img/terraDevastada2.jpeg`);
+            arrayimg.push(`img/terraDevastada3.png`);
+            arrayimg.push(`img/terraDevastada4.jpg`);
+            arrayimg.push(`img/terraDevastada5.jpg`);
+            arrayimg.push(`img/terraDevastada6.jpeg`);
             break;
         case "invasao":
-            conto += `eles chegaram, não deram a minima chance de defesa, a ${obj.tragedia} fez a humanidade de escravo, tomaram o planeta, quem sobreviveu a Guerra e não foi capturado, vive isolado`;
+            conto += `eles chegaram! Não deram a mínima chance de defesa, na ${obj.tragedia} fizeram a humanidade de escravo, tomaram o planeta, quem sobreviveu a Guerra e não foi capturado, vive isolado. Quando todos já nao tinham mais esperença eis que surge uma outra raça alien nos céus, um novo caminho começa para a humanidade e para ${obj.personagem}. `;
+            arrayimg.push(`img/alien1.jpg`);
+            arrayimg.push(`img/alien2.jpg`);
+            arrayimg.push(`img/alien3.jpg`);
+            arrayimg.push(`img/alien4.jpg`);
+            arrayimg.push(`img/alien5.jpg`);
+            arrayimg.push(`img/alien6.jpg`);
             break;
         default:
             break;
     }
+}
+
+function montarArmas(obj){
+    switch (obj.armas) {
+        case "faca":
+            conto += `Com a notícia de esperança se espalhando pelo mundo, pegou sua ${obj.armas}, feita dos metais e pedras mais preciosas, considerada inquebrável, a procura de humanos para lutar e vencer.`;
+            break;
+        case "machado":
+            conto += ` Ao ter a noticia de lutas pela sobrevivência, buscou seu ${obj.armas} forjado pelos anões anciões, partiu para a guerra em busca da liberdade.`;
+            break;
+        case "lanca":
+            conto += `Depois de anos de treino e luta pela sobrevivência, segurou firme sua ${obj.armas}, apontanto diretamente para o inimigo que reluz no horizonte e começa sua marcha pelo destino da Humanidade.`;
+            break;
+        case "metralhadora":
+            conto += `Com a morte de milhares de pessoas, recursos escassos, as únicas coisas que ainda resistem são os resquícios da guerra, ao chegar no forte um dos últimos refugios, encontrou uma ${obj.armas} de calibre .50mm, nomeada de retalhadora, e partiu para a luta onde sobreviver era a unica opção.`;
+            break;
+        default:
+            break;
+    }
+}
+
+function mostraModal(){
+    document.querySelector('header').style.visibility = 'hidden';
+    document.querySelector('main').style.visibility = 'hidden';
+    document.getElementById('fundoModal').style.visibility = 'visible';
+    document.getElementById('divtextConto').style.visibility = 'visible';
+}
+
+function maquinaEscrever(){
+
+    let contoSplit = conto.split('');
+    contoSplit.forEach((element, i) => {
+        setTimeout(() => {
+            textconto.innerHTML += element;
+        }, 80 * i);
+
+        setInterval(() => {
+            montaBackground();
+        },3000);
+    });
+}
+
+function montaBackground(){
+	var randomBack = Math.floor(Math.random() * arrayimg.length);
+  if(randomBack == 0){
+	    randomBack = 1;
+	}
+  document.body.style.backgroundImage = `url('${arrayimg[randomBack]}')`;
 }
